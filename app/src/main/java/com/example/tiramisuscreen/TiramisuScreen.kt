dc080182afbe7922ab131fa6bf5e34bf3a4007a9
KaneLee22 <lzkai@bu.edu>
2025-02-03 20:32:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tiramisuscreen/TiramisuScreen.kt
@@ -0,0 +1,133 @@
+package com.example.tiramisuscreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TiramisuRecipeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        // The title of the screen
+        Text(
+            text = "Tiramisu",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+        // A divider to make tje surface more clear
+        Divider(
+            modifier = Modifier.fillMaxWidth(),
+            color = Color.Gray,
+            thickness = 1.dp
+        )
+        Image(
+            painter = painterResource(id = R.drawable.my_recipe),
+            contentDescription = "Tiramisu Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(vertical = 16.dp)
+        )
+       // Distinguish the ingredients from other parts
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(12.dp),
+            elevation = CardDefaults.cardElevation(4.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5))
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Ingredients:",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "• Mascarpone cheese (300g)")
+                Text(text = "• Heavy cream (250g)")
+                Text(text = "• 3-4 egg yolks")
+                Text(text = "• Water (30g)")
+                Text(text = "• White sugar (50g)")
+                Text(text = "• Ladyfingers")
+                Text(text = "• Coffee liqueur (10g)")
+                Text(text = "• Fresh coffee")
+            }
+        }
+        // Steps to make Tiramisu
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Tutorial",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        // Step1
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Step 1",
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Text(
+            text = "Slowly whip heavy cream + mascarpone cheese.",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        //Step 2
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Step 2",
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Text(
+            text = "Lay ladyfingers in a tray and brush them with coffee. Cover with the cream cheese mixture.",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        //Step 3
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Step 3",
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Text(
+            text = "Add another layer of ladyfingers, top with more cream cheese. Refrigerate and dust with cocoa powder before serving.",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
